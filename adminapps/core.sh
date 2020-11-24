6569c02d9a8419ea9fb6e9cb3fc8a1bfc983a137
Christopher Larson <kergoth@gmail.com>
2020-11-24 10:19:03 -0700

    Use the install-oversight script
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- adminapps/core.sh
+++ adminapps/core.sh
@@ -5,3 +5,5 @@ if [ -d "$CASKROOM"/whatsyoursign ]; then
     find "$CASKROOM"/whatsyoursign -name WhatsYourSign\ Installer.app -print0 \
         | xargs -0 open
 fi
+
+"$(dirname "$0")/../extras/install-oversight"
